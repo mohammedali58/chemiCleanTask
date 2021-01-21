91e1eabead9e67d09665a8f0da1bb1246ab8c351
Mohammed Ali <mohammedali.ibrahim58@gmail.com>
2021-01-22 00:49:44 +0200

    ..

--- ChemiClean/ChemiClean.Core.Test/ProductTest.cs
+++ ChemiClean/ChemiClean.Core.Test/ProductTest.cs
@@ -11,6 +11,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Xunit;
 using static ChemiClean.SharedKernel.SharedKernelEnums;
@@ -19,63 +20,94 @@ namespace Api_Tests
 {
     public class ProductTest
     {
+        
 
         [Fact]
-        public async Task TestProduct_GetAll_ReturnsAListContainsNmeAsync()
+        public async Task GetAllDataSheetsEndPoint()
         {
-            //Arrange
-            var mock = new Mock<IProductRepository>();
-            mock.Setup(repo => repo.GetCountAsync(null)).ReturnsAsync(GetTestProduct().Count);
-
-            var mockMapper = new MapperConfiguration(cfg =>
-            {
-                cfg.AddProfile(new ChemiCleanMapping());
-            });
-
-            var mapper = mockMapper.CreateMapper();
-            var usecase = new ProductGetAllUseCase();
-            OutputPort<ListResultDto<ProductResponseDto>> ProductGetAllPresenter = new OutputPort<ListResultDto<ProductResponseDto>>();
-
-            // Act
-            await usecase.HandleUseCase(ProductGetAllPresenter);
-
-            // Assert
-            var viewResult = Assert.IsType<Task<ListResultDto<ProductResponseDto>>>(ProductGetAllPresenter.Result);
-            var model = Assert.IsAssignableFrom<IEnumerable<ListResultDto<ProductResponseDto>>>(viewResult.Result);
-            Assert.Equal(GetTestProduct().Count, model.Count());
+            var ApiClient = new HttpClient();
+            ApiClient.BaseAddress = new Uri("https://localhost:44381/api/Product/GetAllWithUpdate");
+            HttpResponseMessage response = await ApiClient.GetAsync(ApiClient.BaseAddress);
+            Assert.True(response.IsSuccessStatusCode);
         }
 
         [Fact]
-        public async Task TestProduct_GetAllToShowAsync()
+        public async Task GetAllDatawithoutUpdate()
         {
-            //Arrange
-            var mock = new Mock<IProductRepository>();
-            mock.Setup(repo => repo.GetCountAsync(null)).ReturnsAsync(GetTestProduct().Count);
-
-            var mockMapper = new MapperConfiguration(cfg =>
-            {
-                cfg.AddProfile(new ChemiCleanMapping());
-            });
-
-            var mapper = mockMapper.CreateMapper();
-            var usecase = new ProductGetAllToShowUseCase();
-            OutputPort<ListResultDto<ProductResponseDto>> GetDDLPresenter = new OutputPort<ListResultDto<ProductResponseDto>>();
-
-            // Act
-            await usecase.HandleUseCase(GetDDLPresenter);
-
-            // Assert
-            var viewResult = Assert.IsType<Task<ListResultDto<ProductResponseDto>>>(GetDDLPresenter.Result);
-            var model = Assert.IsAssignableFrom<IEnumerable<ListResultDto<ProductResponseDto>>>(viewResult.Result);
-            Assert.Equal(GetTestProduct().Count, model.Count());
+            var ApiClient = new HttpClient();
+            ApiClient.BaseAddress = new Uri("https://localhost:44381/api/Product/GetAll");
+            HttpResponseMessage response = await ApiClient.GetAsync(ApiClient.BaseAddress);
+            Assert.True(response.IsSuccessStatusCode);
+        }
+        [Fact]
+        public async Task GetSingleDataSheet()
+        {
+            var ApiClient = new HttpClient();
+            ApiClient.BaseAddress = new Uri("https://localhost:44381/api/Product/ShowPDF/GetFile/10");
+            HttpResponseMessage response = await ApiClient.GetAsync(ApiClient.BaseAddress);
+            Assert.True(response.IsSuccessStatusCode);
         }
 
-        private List<Product> GetTestProduct()
-        {
-            return new List<Product> 
-            {
-                new Product{ Id=1 , ProductName="Absol"}
-            };
-        }
+        #region Commented
+
+        //[Fact]
+        //public async Task TestProduct_GetAll_ReturnsAListContainsNmeAsync()
+        //{
+        //    //Arrange
+        //    var mock = new Mock<IProductRepository>();
+        //    mock.Setup(repo => repo.GetCountAsync(null)).ReturnsAsync(GetTestProduct().Count);
+
+        //    var mockMapper = new MapperConfiguration(cfg =>
+        //    {
+        //        cfg.AddProfile(new ChemiCleanMapping());
+        //    });
+
+        //    var mapper = mockMapper.CreateMapper();
+        //    var usecase = new ProductGetAllUseCase();
+        //    OutputPort<ListResultDto<ProductResponseDto>> ProductGetAllPresenter = new OutputPort<ListResultDto<ProductResponseDto>>();
+
+        //    // Act
+        //    await usecase.HandleUseCase(ProductGetAllPresenter);
+
+        //    // Assert
+        //    var viewResult = Assert.IsType<Task<ListResultDto<ProductResponseDto>>>(ProductGetAllPresenter.Result);
+        //    var model = Assert.IsAssignableFrom<IEnumerable<ListResultDto<ProductResponseDto>>>(viewResult.Result);
+        //    Assert.Equal(GetTestProduct().Count, model.Count());
+        //}
+
+        //[Fact]
+        //public async Task TestProduct_GetAllToShowAsync()
+        //{
+        //    //Arrange
+        //    var mock = new Mock<IProductRepository>();
+        //    mock.Setup(repo => repo.GetCountAsync(null)).ReturnsAsync(GetTestProduct().Count);
+
+        //    var mockMapper = new MapperConfiguration(cfg =>
+        //    {
+        //        cfg.AddProfile(new ChemiCleanMapping());
+        //    });
+
+        //    var mapper = mockMapper.CreateMapper();
+        //    var usecase = new ProductGetAllToShowUseCase();
+        //    OutputPort<ListResultDto<ProductResponseDto>> GetDDLPresenter = new OutputPort<ListResultDto<ProductResponseDto>>();
+
+        //    // Act
+        //    await usecase.HandleUseCase(GetDDLPresenter);
+
+        //    // Assert
+        //    var viewResult = Assert.IsType<Task<ListResultDto<ProductResponseDto>>>(GetDDLPresenter.Result);
+        //    var model = Assert.IsAssignableFrom<IEnumerable<ListResultDto<ProductResponseDto>>>(viewResult.Result);
+        //    Assert.Equal(GetTestProduct().Count, model.Count());
+        //}
+
+        //private List<Product> GetTestProduct()
+        //{
+        //    return new List<Product> 
+        //    {
+        //        new Product{ Id=1 , ProductName="Absol"}
+        //    };
+        //}
+        #endregion
+
     }
 }
